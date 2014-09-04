71b11a00c6765e073689bcf4998a34726c948ea3
BiancaVlad <vladbiancalucia@yahoo.com>
2014-09-04 15:06:49 +0300

    a good version of the ball: goes forward and turns and rotates

--- Ball.cpp
+++ Ball.cpp
@@ -1,5 +1,5 @@
 #include "Ball.h"
-
+#include "windows.h"
 
 Ball::~Ball(void)
 {
@@ -8,17 +8,21 @@ Ball::~Ball(void)
 Ball::Ball(float radius, Point center)
 	: Drawable(center)
 {
+	        this->oldY=center.y;
+			this->isUp=false;
+			alphaX=0, alphaY=0, alphaZ=0;
 			float const R = 1./(float)(rings-1);
 			float const S = 1./(float)(sectors-1);
 			int r, s;
-
+			
 			vertices.resize(rings * sectors * 3);
 			normals.resize(rings * sectors * 3);
 			texcoords.resize(rings * sectors * 2);
 			std::vector<GLfloat>::iterator v = vertices.begin();
 			std::vector<GLfloat>::iterator n = normals.begin();
 			std::vector<GLfloat>::iterator t = texcoords.begin();
-			for(r = 0; r < rings; r++) for(s = 0; s < sectors; s++) {
+			for(r = 0; r < rings; r++) 
+				for(s = 0; s < sectors; s++) {
 					float const y = sin( -PI/2 +PI * r * R );
 					float const x = cos(2*PI * s * S) * sin( PI * r * R );
 					float const z = sin(2*PI * s * S) * sin( PI * r * R );
@@ -45,20 +49,83 @@ Ball::Ball(float radius, Point center)
 					*i++ = (r+1) * sectors + s;
 			}
 }
-
 void Ball::Draw()
-    {
+{
         glMatrixMode(GL_MODELVIEW);
         glPushMatrix();
 		glTranslatef(this->center.x, this->center.y, this->center.z);
+		glRotatef(alphaX, 1.0f, 0.0f, 0.0f);
+		glRotatef(alphaY, 0.0f, 1.0f, 0.0f);
+		glRotatef(alphaZ, 0.0f, 0.0f, 1.0f);
 
         glEnableClientState(GL_VERTEX_ARRAY);
         glEnableClientState(GL_NORMAL_ARRAY);
         glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-
         glVertexPointer(3, GL_FLOAT, 0, &vertices[0]);
         glNormalPointer(GL_FLOAT, 0, &normals[0]);
         glTexCoordPointer(2, GL_FLOAT, 0, &texcoords[0]);
         glDrawElements(GL_QUADS, indices.size(), GL_UNSIGNED_SHORT, &indices[0]);
         glPopMatrix();
+}
+
+bool Ball::IsCollision()
+{
+	return false;
+}
+
+void Ball::MoveX(double dx)
+{
+	if(!this->IsCollision())
+	{
+		center.x+=dx;
+		if(dx>0)
+			alphaZ-=10;
+		else
+			alphaZ+=10;
+	}
+}
+
+void Ball::MoveZ(double dz)
+{
+	if(!this->IsCollision())
+	{
+		center.z-=dz;
+		if(dz>0)
+			alphaX-=10;
+		else
+			alphaX+=10;
+	}
+}
+
+void Ball::RotateY(double sy)
+{
+	if(!this->IsCollision())
+			alphaY+=sy;
+}
+
+
+void Ball::Jump(double dy){
+	if(!this->IsCollision())
+	{
+		if(!isUp)
+		{
+			isUp=true;
+			oldY=center.y;
+			center.y+=dy;
+			Jump(-0.002);
+		}
+		else
+		{
+			if(center.y>oldY)
+			{
+			   center.y-=0.002;
+			   Jump(-0.002);
+			}
+		   if(center.y == oldY)
+			   {
+				   isUp=false;
+			   }
+		}
+	}
+
 }
--- Ball.h
+++ Ball.h
@@ -14,8 +14,15 @@ private:
     std::vector<GLushort> indices;
 	static const int rings=50;
 	static const int sectors=100;
+	bool isUp;
+	double oldY, alphaX, alphaY, alphaZ;
 public:
 	~Ball(void);
 	Ball(float radius, Point center);
 	void Draw();
+	bool IsCollision();
+	void MoveX(double dx);
+	void MoveZ(double dz);
+	void RotateY(double sy);
+	void Jump(double dy);
 };
