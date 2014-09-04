eac96fe85bc8148beb67e748cb3bb851b9f661a7
lordarpad6 <k_arpi2004@yahoo.co.uk>
2014-09-04 09:52:12 +0300

    Added Point.

---
+++ Point.cpp
@@ -0,0 +1,7 @@
+#include "Point.h"
+
+Point::Point(double x,double y, double z)
+	:x(x),y(y),z(z)
+{
+}
+
---
+++ Point.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class Point
+{
+	double x,y,z;
+public:
+	Point(double x=0,double y=0,double z=0);
+};
