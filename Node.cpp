783e5d6ff74e86531b35cbe2827c198319aa983c
andu04 <popandi04@gmail.com>
2014-09-04 16:17:15 +0300

    Node with adjacent nodes

--- Node.cpp
+++ Node.cpp
@@ -13,16 +13,19 @@ Node::Node()
 }
 void Node::Draw()
 {
-	glBegin(GL_TRIANGLE_FAN);
+	glBegin(GL_LINE_LOOP);
 	glColor3f(1., 0, 0);
 	glVertex3d(-((center.x - 36) * 10 + WIDTH / 2), center.y, (center.z - 39) * 10 + WIDTH / 2);
 	glVertex3d(-((center.x - 36) * 10 - WIDTH / 2), center.y, (center.z - 39) * 10 + WIDTH / 2);
 	glVertex3d(-((center.x - 36) * 10 - WIDTH / 2), center.y, (center.z - 39) * 10 - WIDTH / 2);
 	glVertex3d(-((center.x - 36) * 10 + WIDTH / 2), center.y, (center.z - 39) * 10 - WIDTH / 2);
 	glEnd();
-}
 
-std::vector<int>& Node::getDestinations()
-{
-	return destinations;
+	/*glBegin(GL_TRIANGLE_FAN);
+	glColor3f(1., 0, 0);
+	glVertex3d(-((center.x - 36) * 10 + WIDTH / 2), center.y, (center.z - 39) * 10 + WIDTH / 2);
+	glVertex3d(-((center.x - 36) * 10 - WIDTH / 2), center.y, (center.z - 39) * 10 + WIDTH / 2);
+	glVertex3d(-((center.x - 36) * 10 - WIDTH / 2), center.y, (center.z - 39) * 10 - WIDTH / 2);
+	glVertex3d(-((center.x - 36) * 10 + WIDTH / 2), center.y, (center.z - 39) * 10 - WIDTH / 2);
+	glEnd();*/
 }
